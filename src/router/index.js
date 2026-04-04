10df5851d2bfef70d0a0340ac0260a128c3bd147
yuei <121796054+Pyrocks123@users.noreply.github.com>
2026-04-04 19:27:19 +0800

    Routing locationdetails

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LandingPage from '@/views/LandingPage.vue';
 import KampongNoticeboard from '@/views/KampongNoticeboard.vue';
+import LocationDetails from '@/views/LocationDetails.vue';   
 
 const routes = [
   { path: '/', name: 'Landing', component: LandingPage },
   { path: '/noticeboard', name: 'KampongNoticeboard', component: KampongNoticeboard },
+  
+  { 
+    path: '/location/:id', 
+    name: 'LocationDetails', 
+    component: LocationDetails 
+  }
 ];
 
 const router = createRouter({
